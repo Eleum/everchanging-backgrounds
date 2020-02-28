f55c8a53bab044707eba8ce24c7caff9b133382e
Dmitry Vasilevich <wotlk777@gmail.com>
2020-02-29 01:56:23 +0300

    play more with timed messages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ElectronService } from '../core/services/electron/electron.service';
 import { HttpClient } from '@angular/common/http';
-import { threadId } from 'worker_threads';
+import { Subject } from 'rxjs';
 
 declare var VK: any;
+declare var document: any;
 declare var window: any;
 
 @Component({
@@ -13,18 +14,25 @@ declare var window: any;
 })
 export class HomeComponent implements OnInit {
     accessToken = "";
-    messages: string[] = [];
+
+    messages: object[] = [];
+    messageNotifier = new Subject<string>();
 
     timers = {};
+    timersFinished = false;
     addTimer: Function;
-    removeTimer: Function;
 
     constructor(private http: HttpClient, private electron: ElectronService) { }
 
     ngOnInit() {
         let timers = this.timers;
+        let messages = this.messages;
+        let shouldSeparateMessages = false;
+        let separateMessagesTimer: NodeJS.Timeout;
 
-        this.addTimer = function(fn, delay) {
+        this.messageNotifier.subscribe({ next: (value) => addMessage(value) });
+
+        this.addTimer = function (fn, delay) {
             var id = window.setTimeout(() => {
                 fn();
                 delete timers[id];
@@ -35,24 +43,29 @@ export class HomeComponent implements OnInit {
         }
 
         var _clearTimeout = window.clearTimeout;
-        window.clearTimeout = function(id) {
+        window.clearTimeout = function (id) {
             delete timers[id];
             _clearTimeout(id);
         }
 
-        setTimeout(() => {
-            let listener = setInterval(() => {
-                if (!this.activeTimers()) {
-                    this.messages.push('finished');
-                    clearTimeout(listener);
-                }
-            }, 100);
-        }, 0);
+        function addMessage(message: string) {
+            messages.push({ shouldSeparateMessages, message, firstMessage: messages.length === 0 });
+            
+            clearTimeout(separateMessagesTimer);
+            shouldSeparateMessages = false;
+            separateMessagesTimer = setTimeout(() => {
+                shouldSeparateMessages = true;
+            }, 4000);
+        }
+
+        function activeTimers() {
+            return Object.keys(timers).length > 0;
+        }
 
         let i = 0;
         function increment(messages: string[]) {
             i++;
-            messages.push("this is " + i.toString());
+            addMessage("this is " + i.toString());
         }
 
         let timer = setTimeout(function sendTimedMessage(messages: string[]) {
@@ -60,9 +73,9 @@ export class HomeComponent implements OnInit {
             timer = setTimeout(sendTimedMessage, 1000, messages);
         }, 0, this.messages);
 
-        this.addTimer(() => { clearTimeout(timer) }, 7000);
-        this.addTimer(() => {this.messages.push('1')}, 3000);
-
+        this.addTimer(() => { clearTimeout(timer) }, 2000);
+        this.addTimer(() => { addMessage('42') }, 7000);
+        
         // var BrowserWindow = this.electron.remote.BrowserWindow;
 
         // const authUrl = 'https://oauth.vk.com/authorize?client_id=&display=popup&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,status&response_type=token&v=5.103&state=123456&revoke=1';
@@ -92,19 +105,21 @@ export class HomeComponent implements OnInit {
         // authWindow.show();
     }
 
+    public sendTimedMessage(e: any) {
+        if (!e) {
+            return;
+        }
+
+        document.getElementById('message-input').value = '';
+        this.timersFinished = false;
+        this.addTimer(() => { this.messageNotifier.next(e); }, 3000);
+    }
+
     public minimizeWindow() {
         this.electron.window.minimize();
     }
 
     public closeWindow() {
         this.electron.window.close();
-    }
-
-    public sendTestMessage(e: any) {
-        debugger;
-    }
-
-    private activeTimers() {
-        return Object.keys(this.timers).length > 0;
     }
 }
