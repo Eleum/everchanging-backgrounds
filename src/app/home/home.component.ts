16006f2fcd89f9177db02358bbead744e3fc344d
Dmitry Vasilevich <wotlk777@gmail.com>
2020-02-28 23:34:34 +0300

    some timeouts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ElectronService } from '../core/services/electron/electron.service';
 import { HttpClient } from '@angular/common/http';
+import { threadId } from 'worker_threads';
 
 declare var VK: any;
+declare var window: any;
 
 @Component({
     selector: 'app-home',
@@ -11,38 +13,83 @@ declare var VK: any;
 })
 export class HomeComponent implements OnInit {
     accessToken = "";
+    messages: string[] = [];
+
+    timers = {};
+    addTimer: Function;
+    removeTimer: Function;
 
     constructor(private http: HttpClient, private electron: ElectronService) { }
 
-    ngOnInit(): void {
-        return;
-        var BrowserWindow = this.electron.remote.BrowserWindow;
+    ngOnInit() {
+        let timers = this.timers;
 
-        const authUrl = 'https://oauth.vk.com/authorize?client_id=&display=popup&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,status&response_type=token&v=5.103&state=123456&revoke=1';
+        this.addTimer = function(fn, delay) {
+            var id = window.setTimeout(() => {
+                fn();
+                delete timers[id];
+            }, delay);
 
-        let authWindow = new BrowserWindow({ width: 800, height: 600, show: false, webPreferences: { nodeIntegration: false } });
+            timers[id] = true;
+            return id;
+        }
 
-        authWindow.webContents.on('will-redirect', (event, url) => {
-            const parts = url.split('#');
-            const params = parts[1]
-                .split('&')
-                .map(p => p.split('='))
-                .reduce((obj, pair) => {
-                    const [key, value] = pair.map(decodeURIComponent);
-                    return ({ ...obj, [key]: value })
-                }, {});
+        var _clearTimeout = window.clearTimeout;
+        window.clearTimeout = function(id) {
+            delete timers[id];
+            _clearTimeout(id);
+        }
 
-            this.accessToken = params["access_token"];
+        setTimeout(() => {
+            let listener = setInterval(() => {
+                if (!this.activeTimers()) {
+                    this.messages.push('finished');
+                    clearTimeout(listener);
+                }
+            }, 100);
+        }, 0);
 
-            authWindow.close();
-        });
+        let i = 0;
+        function increment(messages: string[]) {
+            i++;
+            messages.push("this is " + i.toString());
+        }
 
-        authWindow.on('close', () => {
-            authWindow = null;
-        });
+        let timer = setTimeout(function sendTimedMessage(messages: string[]) {
+            increment(messages);
+            timer = setTimeout(sendTimedMessage, 1000, messages);
+        }, 0, this.messages);
 
-        authWindow.loadURL(authUrl);
-        authWindow.show();
+        this.addTimer(() => { clearTimeout(timer) }, 7000);
+        this.addTimer(() => {this.messages.push('1')}, 3000);
+
+        // var BrowserWindow = this.electron.remote.BrowserWindow;
+
+        // const authUrl = 'https://oauth.vk.com/authorize?client_id=&display=popup&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,status&response_type=token&v=5.103&state=123456&revoke=1';
+
+        // let authWindow = new BrowserWindow({ width: 800, height: 600, show: false, webPreferences: { nodeIntegration: false } });
+
+        // authWindow.webContents.on('will-redirect', (event, url) => {
+        //     const parts = url.split('#');
+        //     const params = parts[1]
+        //         .split('&')
+        //         .map(p => p.split('='))
+        //         .reduce((obj, pair) => {
+        //             const [key, value] = pair.map(decodeURIComponent);
+        //             return ({ ...obj, [key]: value })
+        //         }, {});
+
+        //     this.accessToken = params["access_token"];
+
+        //     authWindow.close();
+        // });
+
+        // authWindow.on('close', () => {
+        //     authWindow = null;
+        // });
+
+        // authWindow.loadURL(authUrl);
+        // authWindow.show();
     }
 
     public minimizeWindow() {
@@ -55,5 +102,9 @@ export class HomeComponent implements OnInit {
 
     public sendTestMessage(e: any) {
         debugger;
+    }
+
+    private activeTimers() {
+        return Object.keys(this.timers).length > 0;
     }
 }
