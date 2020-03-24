2f4f3a022cffcb0a92ec3f078420b4d4024a0d0b
Dmitry Vasilevich <wotlk777@gmail.com>
2020-03-25 01:49:07 +0300

    fix carousel display

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -1,40 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { NguCarouselConfig } from '@ngu/carousel';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { NguCarouselConfig, NguCarouselStore } from '@ngu/carousel';
 
 @Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+    selector: 'app-carousel',
+    templateUrl: './carousel.component.html',
+    styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
-  imgags = [
-    './assets/bg.jpg',
-    './assets/bg1.jpg'
-  ];
-  carouselInstances: Array<any> = [0];
-  carouselTiles = { 0: [] };
-  tile: NguCarouselConfig = {
-    grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
-    slide: 1,
-    speed: 950,
-    point: {
-      visible: false
-    },
-    loop: false,
-    velocity: 0,
-    touch: true,
-    easing: 'cubic-bezier(0, 0, 0.2, 1)'
-  };
+export class CarouselComponent implements OnInit, AfterViewInit {
+    images = [
+        './assets/bg.jpg',
+        './assets/bg1.jpg'
+    ];
+    tiles = [];
+    tileConfig: NguCarouselConfig = {
+        grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+        slide: 1,
+        speed: 850,
+        point: {
+            visible: false
+        },
+        loop: false,
+        velocity: 0,
+        touch: true,
+        easing: 'cubic-bezier(0, 0, 0.2, 1)'
+    };
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-    this.carouselInstances.forEach(el => {
-      this.loadCarouselTile(el);
-    });
-  }
+    ngOnInit() {
+        this.loadCarouselSlides();
+    }
 
-  public loadCarouselTile(carouselIdx: number) {
-    this.carouselTiles[carouselIdx] = this.carouselTiles[carouselIdx].concat(this.imgags);
-  }
+    ngAfterViewInit() {
+        this.setElementsStyle();
+    }    
+
+    public slideChange(data: NguCarouselStore) {
+        console.log(data);
+    }
+
+    private loadCarouselSlides() {
+        this.tiles = this.tiles.concat(this.images);
+    }
+
+    private setElementsStyle() {
+        (<any>document.getElementsByClassName('ngu-touch-container')[0]).style.height = '100%';
+        Array.from(document.getElementsByClassName('tile')).forEach((element: any) => {
+            element.style.height = '100%';
+        });
+    }
 }
